5ee02363324990b22476071744e69d6ba5a65689
Cesar Daldosso <cesardaldosso@gmail.com>
2025-06-14 17:57:03 -0300

    termina

--- Program.cs
+++ Program.cs
@@ -5,34 +5,31 @@ class URI
 
     static void Main(string[] args)
     {
-        int XImpar, YImpar, i, menor, maior,soma,valor1,valor2;
         int X = int.Parse(Console.ReadLine());
         int Y = int.Parse(Console.ReadLine());
-        // determinar o numero maior entre X e Y
-        soma = 0;
-        valor1 = 0;
-        valor2 = 0;
+        int Resto,maior, menor,Soma;
+        Soma = 0;
         if (X > Y)
         {
             maior = X;
             menor = Y;
         }
         else
-        {
-            maior = Y;
             menor = X;
-        }
-
-       
-        for (i=menor+1; i<maior; i++)
+        maior = X;
+        do
         {
-
-            if (i % 2 != 0)
-            {
-                
-                soma=i;
-            }
+           
+            Resto = menor % 2;
+            if (Resto==0)
+            menor++;
+            else
+                Soma = menor + Soma;
+            menor++;
         }
-        Console.WriteLine(soma);
+        while (menor < maior);
+        Console.WriteLine(Soma);
+
     }
 }
+
