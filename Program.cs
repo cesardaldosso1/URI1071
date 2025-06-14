9f09ab21101de186462a1190c26cfccfa244cc44
Cesar Daldosso <cesardaldosso@gmail.com>
2025-06-14 13:50:27 -0300

    new commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int XImpar, YImpar, i, menor, maior,soma,valor1,valor2;
+        int X = int.Parse(Console.ReadLine());
+        int Y = int.Parse(Console.ReadLine());
+        // determinar o numero maior entre X e Y
+        soma = 0;
+        valor1 = 0;
+        valor2 = 0;
+        if (X > Y)
+        {
+            maior = X;
+            menor = Y;
+        }
+        else
+        {
+            maior = Y;
+            menor = X;
+        }
+
+       
+        for (i=menor+1; i<maior; i++)
+        {
+
+            if (i % 2 != 0)
+            {
+                
+                soma=i;
+            }
+        }
+        Console.WriteLine(soma);
+    }
+}
